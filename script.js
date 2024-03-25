d969afb2e7df15cfb5923897155d39ce25674b67
MihasTJ <michal.janiol00@gmail.com>
2024-03-25 17:51:42 +0100

    assets, new sections

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const tabLinks = document.querySelectorAll(".tab-links");
 const tabContents = document.querySelectorAll(".tab-contents");
+const btnBurger = document.querySelector('.burgerbtn');
+const navMenu = document.querySelector('#navmenu');
+const navMenuItems = document.querySelectorAll('#navmenu li');
 
 const openLink = (index) => {
     // Remove "active-link" class from all tabLinks elements
@@ -25,3 +28,26 @@ tabLinks.forEach((link, index) => {
         openLink(index);
     });
 });
+
+btnBurger.addEventListener('click',()=>{
+    btnBurger.classList.toggle('active-menu');
+    navMenu.style.right = navMenu.style.right !== "0px" ? "0px" : "-200px";
+})
+navMenuItems.forEach((element) =>{
+    element.addEventListener('click',()=>{
+        navMenu.style.right = "-200px";
+    })
+})
+
+
+// -------------- API contact------------------
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzqRGsW1lW77dMuQbj4JCVCl7-EIpMNpbxY49z2nC9CH5J9kE0fAFGXlvnsv35QAPBD/exec'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+e.preventDefault()
+fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message))
+})
