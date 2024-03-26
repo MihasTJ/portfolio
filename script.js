93e299a1af24973faaca1d1c4f7db2452dcff4a0
MihasTJ <michal.janiol00@gmail.com>
2024-03-26 13:11:16 +0100

    new change

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const btnBurger = document.querySelector('.burgerbtn');
 const navMenu = document.querySelector('#navmenu');
 const navMenuItems = document.querySelectorAll('#navmenu li');
 
+// ------------- about section -----------------
 const openLink = (index) => {
     // Remove "active-link" class from all tabLinks elements
     tabLinks.forEach(element => {
@@ -29,6 +30,7 @@ tabLinks.forEach((link, index) => {
     });
 });
 
+// ------------- burger menu -----------------
 btnBurger.addEventListener('click',()=>{
     btnBurger.classList.toggle('active-menu');
     navMenu.style.right = navMenu.style.right !== "0px" ? "0px" : "-200px";
@@ -40,14 +42,43 @@ navMenuItems.forEach((element) =>{
 })
 
 
-// -------------- API contact------------------
+// -------------- form contact------------------
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzqRGsW1lW77dMuQbj4JCVCl7-EIpMNpbxY49z2nC9CH5J9kE0fAFGXlvnsv35QAPBD/exec'
-const form = document.forms['submit-to-google-sheet']
+const scriptURL = 'https://script.google.com/macros/s/AKfycbx9FAmUsy_cTlHi8_EUH1f0gVAV2VwmDFygJ7X645l31H2p-afqV37RvcCJuD4lJ5Oa/exec';
+const form = document.forms['submit-to-google-sheet'];
+const msg = document.getElementById("msg");
+const variables = document.querySelectorAll('form[name="submit-to-google-sheet"] input, form[name="submit-to-google-sheet"] textarea');
 
 form.addEventListener('submit', e => {
-e.preventDefault()
-fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => console.log('Success!', response))
-    .catch(error => console.error('Error!', error.message))
-})
+    e.preventDefault();
+
+    // Check if all fields are filled
+    let allFieldsFilled = true;
+    variables.forEach(variable => {
+        if (variable.value.trim() === '') {
+            allFieldsFilled = false;
+            return;
+        }
+    });
+    if (!allFieldsFilled) {
+        msg.innerHTML = "Fill in all fields";
+        return;
+    }
+    // All fields are filled, submit the form
+    msg.classList.remove('error', 'successfull');
+    msg.innerHTML = "Sending...";
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.classList.add('successfull');
+            msg.innerHTML = "Message sent successfully!";
+            setTimeout(function () {
+                msg.innerHTML = "";
+            }, 5000);
+            form.reset();
+        })
+        .catch(error => {
+            msg.classList.add('error');
+            msg.innerHTML = "An error occurred while sending the message!";
+        });
+});
+
